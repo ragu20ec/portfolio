732c0c180266816c4baca82985c22b9313b0f9e2
ragu20ec <86743355+ragu20ec@users.noreply.github.com>
2022-07-24 16:26:57 +0530

    Add files via upload

---
+++ homepage/script.js
@@ -0,0 +1,19 @@
+function resume() {
+    $.ajax({
+        url: 'RESUME.pdf',
+        method: 'GET',
+        xhrFields: {
+            responseType: 'blob'
+        },
+        success: function (data) {
+            var a = document.createElement('a');
+            var url = window.URL.createObjectURL(data);
+            a.href = url;
+            a.download = 'myfile.pdf';
+            document.body.append(a);
+            a.click();
+            a.remove();
+            window.URL.revokeObjectURL(url);
+        }
+    });
+};
